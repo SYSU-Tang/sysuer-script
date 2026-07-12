1a4357c3bef6074d92a8badfd956faf9386d6bf8
SYSU-Tang <2658481919@qq.com>
2026-07-12 16:25:23 +0800

    init

---
+++ sysuer.meta.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         SYSUER美化辅助增强
+// @namespace    https://github.com/SYSU-Tang
+// @version      1.2
+// @description  中大儿增强脚本，包括网页净化、在线教学平台视频自动速通、自动跳下一页、自动登录、跳过验证、自动跳转登录页。
+// @author       SYSU-Tang
+// @match        *://www.sysu.edu.cn/*
+// @match        *://jwxt.sysu.edu.cn/*
+// @match        *://portal.sysu.edu.cn/*
+// @match        *://cas.sysu.edu.cn/esc-sso/login/page
+// @match        *://lms.sysu.edu.cn/*
+// @match        *://cas.sysu.edu.cn/*
+// @match        *://appgw.sysu.edu.cn/*
+// @match        *://visitor.sysu.edu.cn/*
+// @match        *://visitor-443.webvpn.sysu.edu.cn/*
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_registerMenuCommand
