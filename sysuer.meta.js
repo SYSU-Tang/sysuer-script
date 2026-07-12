96475775c49b1e02618bb4decea6dad1dcf11bbf
SYSU-Tang <2658481919@qq.com>
2026-07-12 16:34:45 +0800

    init

--- sysuer.meta.js
+++ sysuer.meta.js
@@ -4,6 +4,7 @@
 // @version      1.2
 // @description  中大儿增强脚本，包括网页净化、在线教学平台视频自动速通、自动跳下一页、自动登录、跳过验证、自动跳转登录页。
 // @author       SYSU-Tang
+// @homepage     https://github.com/SYSU-Tang/sysuer.user.js
 // @match        *://www.sysu.edu.cn/*
 // @match        *://jwxt.sysu.edu.cn/*
 // @match        *://portal.sysu.edu.cn/*
@@ -16,3 +17,4 @@
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_registerMenuCommand
+// ==/UserScript==
--- sysuer.user.js
+++ sysuer.user.js
@@ -4,6 +4,9 @@
 // @version      1.2
 // @description  中大儿增强脚本，包括网页净化、在线教学平台视频自动速通、自动跳下一页、自动登录、跳过验证、自动跳转登录页。
 // @author       SYSU-Tang
+// @updateURL    https://github.com/SYSU-Tang/sysuer.user.js/raw/main/sysuer.meta.js
+// @downloadURL  https://github.com/SYSU-Tang/sysuer.user.js/raw/main/sysuer.user.js
+// @homepage     https://github.com/SYSU-Tang/sysuer.user.js
 // @match        *://www.sysu.edu.cn/*
 // @match        *://jwxt.sysu.edu.cn/*
 // @match        *://portal.sysu.edu.cn/*
